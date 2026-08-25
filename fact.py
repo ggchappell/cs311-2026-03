e712ce24b4d3dafb3ae0dde539074988d5da6205
Glenn G. Chappell <ggchappell@alaska.edu>
2026-08-24 19:05:13 -0800

    Add fact.py

---
+++ fact.py
@@ -0,0 +1,21 @@
+# fact.py
+# Glenn G. Chappell
+# 2026-08-24
+"""First Python program: compute factorials"""
+
+
+def fact(n):
+    """Return the factorial of n."""
+    result = 1
+    for i in range(1, 1+n):
+        result *= i
+    return result
+
+
+k = 4
+print(k, "factorial:", fact(k))
+
+k = 100
+print(k, "factorial:", fact(k))
+
+
