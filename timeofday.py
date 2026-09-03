f3b9145fc781e1142fe96e0f368bec16aafd4866
Glenn G. Chappell <ggchappell@alaska.edu>
2026-09-02 17:02:49 -0800

    timeofday.py: FINISH - write ops, pretty up everything

--- timeofday.py
+++ timeofday.py
@@ -1,39 +1,58 @@
-# timeofday.py  UNFINISHED
+# timeofday.py
 # Glenn G. Chappell
-# 2026-08-31
+# Started: 2026-08-31
+# Updated: 2026-09-02
 """Class TimeOfDay: store & manipulate a time of day.
 For CS 311 Fall 2026
 """
 
+import copy  # For .deepcopy
+
+
+# **********************************************************************
+# Class TimeOfDay - Internal-use constants
+# **********************************************************************
+
+
+_SECS_IN_MIN = 60
+_SECS_IN_HR = 60*_SECS_IN_MIN
+_SECS_IN_DAY = 24*_SECS_IN_HR
+
+
+# **********************************************************************
+# Class TimeOfDay - Class definition
+# **********************************************************************
+
 
 class TimeOfDay:
-    # Attributes:
-    # - _secs: seconds past midnight - must be int from 0 to 24*60*60-1
+    # ***** TimeOfDay: List of attributes *****
 
-    # Initializer
+    # - _secs: seconds past midnight - must be int in [0, _SECS_IN_DAY).
+
+    # ***** TimeOfDay: Initializer *****
 
     def __init__(self, hh=None, mm=None, ss=None):
         """Set time given hrs, mins, secs, or midnight if not given."""
-        if hh is None:
+
+        if hh is not None:
+            assert isinstance(hh, int)
+            assert isinstance(mm, int)
+            assert isinstance(ss, int)
+        else:
             hh = 0
-        if mm is None:
             mm = 0
-        if ss is None:
             ss = 0
-        assert isinstance(hh, int)
-        assert isinstance(mm, int)
-        assert isinstance(ss, int)
 
         self.set_time(hh, mm, ss)
 
-    # Normal methods
+    # ***** TimeOfDay: Normal methods *****
 
     def get_time(self):
         """Return tuple holding hrs, mins, secs past midnight."""
 
-        hh = self._secs // 60*60
-        mm = (self._secs - hh*60*60) // 60
-        ss = self._secs - hh*60*60 - mm*60
+        hh = self._secs // _SECS_IN_HR
+        mm = (self._secs - hh*_SECS_IN_HR) // _SECS_IN_MIN
+        ss = self._secs - hh*_SECS_IN_HR - mm*_SECS_IN_MIN
         return (hh, mm, ss)
 
     def set_time(self, hh, mm, ss):
@@ -43,36 +62,64 @@ class TimeOfDay:
         assert isinstance(mm, int)
         assert isinstance(ss, int)
 
-        self._secs = hh*60*60 + mm*60 + ss
+        self._secs = hh*_SECS_IN_HR + mm*_SECS_IN_MIN + ss
         self._fix_secs()
 
-    # Operators & string conversion
+    # ***** TimeOfDay: Operators & string conversion *****
 
+    # __add__: binary + operator
     def __add__(self, chg):
-        return self  # Dummy
+        """Return new object advanced by chg seconds."""
 
+        assert isinstance(chg, int)
+
+        cpy = copy.deepcopy(self)
+        cpy += chg
+        return cpy
+
+    # __sub__: binary - operator
     def __sub__(self, chg):
-        return self  # Dummy
+        """Return new object reduced by chg seconds."""
 
+        assert isinstance(chg, int)
+
+        cpy = copy.deepcopy(self)
+        cpy -= chg
+        return cpy
+
+    # __iadd__: += operator
     def __iadd__(self, chg):
-        return self  # Dummy
+        """Advance current object by chg seconds."""
 
+        assert isinstance(chg, int)
+
+        self._secs += chg
+        self._fix_secs()
+        return self
+
+    # __isub__: -= operator
     def __isub__(self, chg):
-        return self  # Dummy
+        """Reduce current object by chg seconds."""
+
+        assert isinstance(chg, int)
+
+        self._secs -= chg
+        self._fix_secs()
+        return self
 
     def __str__(self):
         """Informal string conversion: ' 8:30:05'."""
-        hms = self.get_time()
+        hh, mm, ss = self.get_time()
         result = ""
-        if hms[0] < 10:
+        if hh < 10:
             result = " "
-        result += str(hms[0]) + ":"
-        if hms[1] < 10:
+        result += str(hh) + ":"
+        if mm < 10:
             result += "0"
-        result += str(hms[1]) + ":"
-        if hms[2] < 10:
+        result += str(mm) + ":"
+        if ss < 10:
             result += "0"
-        result += str(hms[2])
+        result += str(ss)
         return result
 
     def __repr__(self):
@@ -80,11 +127,14 @@ class TimeOfDay:
 
         return str(self)
 
-    # Private methods
+    # ***** TimeOfDay: Private methods *****
 
     def _fix_secs(self):
         """Private. Ensure attribute _secs lies in range."""
 
-        if self._secs >= 24*60*60 or self._secs < 0:
-            self._secs %= 24*60*60  # Does right thing when _secs < 0
+        if self._secs >= _SECS_IN_DAY or self._secs < 0:
+            self._secs %= _SECS_IN_DAY
+            # Above line does the right thing when _secs < 0
+
+# End class TimeOfDay
 
