f4cc10037c5d80b5d4eddfb224a6e0aaed929243
Glenn G. Chappell <ggchappell@alaska.edu>
2026-09-21 20:34:44 -0800

    Add da3.py (SKELETON)

---
+++ da3.py
@@ -0,0 +1,44 @@
+# da3.py  SKELETON
+# Glenn G. Chappell
+# 2026-09-21
+"""Answers to CS 311 Assignment 3, Exercises A-D."""
+
+
+import llnode  # For _LLNode
+
+
+def ll_iter(the_linked_list):
+    """DOCSTRING NEEDED."""
+
+    return [1, 2, 3]  # Dummy
+    # TODO: WRITE THIS!!!
+
+
+def create_ll(the_iterable):
+    """DOCSTRING NEEDED."""
+
+    return None  # Dummy
+    # TODO: WRITE THIS!!!
+
+
+def does_it_raise(the_callable):
+    """DOCSTRING NEEDED."""
+
+    return False  # Dummy
+    # TODO: WRITE THIS!!!
+
+
+def mcss(the_indexable, index1=None, index2=None):
+    """DOCSTRING NEEDED."""
+
+    if index1 is None:
+        index1 = 0
+    assert isinstance(index1, int)
+    if index2 is None:
+        index2 = len(the_indexable)
+    assert isinstance(index2, int)
+    assert index2 >= index1
+
+    return (1, 2, 3, 4)  # Dummy
+    # TODO: WRITE THIS!!!
+
